2a36d247690371e26d8ccdade3bceafb2ed5df19
adrianncovaci <adrianmihaicovaci@gmail.com>
2021-11-17 23:57:27 +0200

    bumped blake version to 0.9.2, storing text as hex

--- nftrader_image_process/src/server.rs
+++ nftrader_image_process/src/server.rs
@@ -16,25 +16,22 @@ use tonic::{Request, Response, Status};
 impl ImageGuid for ImageGuidProcess {
     async fn get_image(&self, _request: Request<Image>) -> Result<Response<Image>, Status> {
         let connection = establish_connection();
-        println!("aici");
-        if let Some(image) = nftrader_image_process::database::imagequeries::get_image(
-            &connection,
-            _request.get_ref().base64.as_str(),
-        ) {
+        let mut hasher = Blake2b::new();
+        let data = _request.get_ref().base64.as_bytes().to_vec();
+        hasher.update(data);
+        let hashed = format!("{:x}", hasher.finalize());
+        if let Some(image) =
+            nftrader_image_process::database::imagequeries::get_image(&connection, &hashed)
+        {
             let response_image = Image {
                 base64: image.hashed_value,
             };
             return Ok(Response::new(response_image));
         } else {
-            println!("here");
-            let mut hasher = Blake2b::new();
-            let data = _request.get_ref().base64.as_bytes().to_vec();
-            hasher.input(data);
-            let hashed = format!("{:?}", hasher.result());
-            println!("{}", hashed);
-            let hashed = hashed.as_str();
-            let new_image =
-                nftrader_image_process::database::imagemutations::create_image(&connection, hashed);
+            let new_image = nftrader_image_process::database::imagemutations::create_image(
+                &connection,
+                &hashed,
+            );
             let response_image = Image {
                 base64: new_image.hashed_value,
             };
